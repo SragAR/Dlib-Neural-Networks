ec1ef342d7c9f6c879e273e2352f4522f7c667c5
sreeragh <sreeragh@qburst.com>
2018-05-08 16:46:53 +0530

    simple relu applied on an image

--- my_net.cpp
+++ my_net.cpp
@@ -1,7 +1,11 @@
 #include <dlib/gui_widgets.h>
 #include <dlib/image_io.h>
 #include <dlib/image_transforms.h>
+// #include "dlib/matrix.h"
 #include <iostream>
+#include <dlib/dnn.h>
+#include <iostream>
+#include <dlib/data_io.h>
 
 using namespace std;
 using namespace dlib;
@@ -17,12 +21,16 @@ int main(int argc, char **argv)
             cout << "error, you have to enter a BMP file as an argument to this program" << endl;
             return 1;
         }
-        array2d<rgb_pixel> img;
-        load_image(img, argv[1]);
-        array2d<unsigned char> blurred_img;
-        gaussian_blur(img, blurred_img);
-        image_window my_window(blurred_img, "Blurred Image");
+        using my_net_type = relu<input<array2d<rgb_pixel>>>;
+        my_net_type net;
+        array2d<rgb_pixel> img_mine, out_img_mine;
+        load_image(img_mine, argv[1]);
+        cout<<"Net details"<<net(img_mine).nr()<<" "<<net(img_mine).nc();
+        image_window my_window(mat(net(img_mine),150,150 ), "Convolved Image");
+        image_window my_window1(mat(img_mine), "Original Image");
         my_window.wait_until_closed();
+        my_window1.wait_until_closed();
+
     }
     catch (exception &e)
     {
