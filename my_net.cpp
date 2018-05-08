184afc9ae7db0dfacd3a804c3e5ff2ced2cc5450
sreeragh <sreeragh@qburst.com>
2018-05-08 17:16:48 +0530

    a simple maxpool implemented

--- my_net.cpp
+++ my_net.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 using namespace dlib;
 
+
 int main(int argc, char **argv)
 {
 
@@ -21,15 +22,23 @@ int main(int argc, char **argv)
             cout << "error, you have to enter a BMP file as an argument to this program" << endl;
             return 1;
         }
-        using my_net_type = relu<input<array2d<rgb_pixel>>>;
-        my_net_type net;
+        using my_relu_net_type = relu<input<array2d<rgb_pixel>>>;
+        my_relu_net_type relu_net;
+        using my_max_pool_type = max_pool<2,2,2,2,input<array2d<rgb_pixel>>>;
+        my_max_pool_type max_pool_net;
         array2d<rgb_pixel> img_mine, out_img_mine;
         load_image(img_mine, argv[1]);
-        cout<<"Net details"<<net(img_mine).nr()<<" "<<net(img_mine).nc();
-        image_window my_window(mat(net(img_mine),150,150 ), "Convolved Image");
+        cout<<"Net details"<<relu_net(img_mine).nr()<<" "<<relu_net(img_mine).nc();
+        image_window my_window(mat(relu_net(img_mine),150,150 ), "Convolved Image");
+        long n_rows, n_columns;
+        n_rows = max_pool_net(img_mine).nr();
+        n_columns = max_pool_net(img_mine).nc();
+        cout<<"Maxpool  details"<<max_pool_net(img_mine).nr()<<" "<<max_pool_net(img_mine).nc();
+        image_window my_window2(mat(max_pool_net(img_mine),n_rows,n_columns), "MaxPool Image");
         image_window my_window1(mat(img_mine), "Original Image");
         my_window.wait_until_closed();
         my_window1.wait_until_closed();
+        my_window2.wait_until_closed();
 
     }
     catch (exception &e)
