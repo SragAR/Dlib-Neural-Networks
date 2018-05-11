f34c86956438ee6aef13483d6106d71aff7d3d06
sreeragh <sreeraghar@gmail.com>
2018-05-11 17:02:50 +0530

    residual nets defined

--- my_net.cpp
+++ my_net.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 using namespace dlib;
 
-
 int main(int argc, char **argv)
 {
 
@@ -24,26 +23,33 @@ int main(int argc, char **argv)
         }
         using my_relu_net_type = relu<input<array2d<rgb_pixel>>>;
         my_relu_net_type relu_net;
-        using my_max_pool_type = max_pool<2,2,2,2,input<array2d<rgb_pixel>>>;
+        using my_max_pool_type = max_pool<2, 2, 2, 2, input<array2d<rgb_pixel>>>;
         my_max_pool_type max_pool_net;
-        using fully_connected_type = fc_no_bias<128,input<array2d<rgb_pixel>>>;
+        using fully_connected_type = fc_no_bias<128, input<array2d<rgb_pixel>>>;
         fully_connected_type full_net;
 
-        using mock_resnet_type = con<32,7,7,2,2,input<array2d<rgb_pixel>>>;
+        template <int N, typename SUBNET>
+        using ares = relu<residual<block, N, affine, SUBNET>>;
+        template <int N, typename SUBNET>
+        using ares_down = relu<residual_down<block, N, affine, SUBNET>>;
+        template <int N, template <typename> class BN, int stride, typename SUBNET>
+        using block = BN<con<N, 3, 3, 1, 1, relu<BN<con<N, 3, 3, stride, stride, SUBNET>>>>>;
+
+        using mock_resnet_type = max_pool<3, 3, 2, 2, relu<affine<con<32, 7, 7, 2, 2, input<array2d<rgb_pixel>>>>>>;
         mock_resnet_type mock_resnet;
 
-        array2d<rgb_pixel> img_mine,out_img;
+        array2d<rgb_pixel> img_mine, out_img;
         load_image(img_mine, argv[1]);
         long n_rows, n_columns;
         resizable_tensor resnet_output;
         resnet_output = mock_resnet(img_mine);
         n_rows = resnet_output.nr();
         n_columns = resnet_output.nc();
-        image_window my_window2(mat(resnet_output,n_rows,n_columns), "Resnet Output");
+        cout << n_rows << " " << n_columns;
+        image_window my_window2(mat(resnet_output, n_rows, n_columns), "Resnet Output");
         image_window my_window1(mat(img_mine), "Original Image");
         my_window1.wait_until_closed();
         my_window2.wait_until_closed();
-
     }
     catch (exception &e)
     {
